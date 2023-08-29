1909cf7dacaa8cfd19ab2543e86f52b4be84b28c
Tomás Rodeghiero <tomirodeghiero@Tomass-MacBook-Air.local>
2023-08-29 09:54:28 -0300

    Update categories

--- client/src/utils/constants/index.ts
+++ client/src/utils/constants/index.ts
@@ -1,8 +1,29 @@
-// Categories
+// Categories: Joyas Boulevard
+// export const CATEGORIES = [
+//   "Aros",
+//   "Cadenas",
+//   "Conjuntos",
+//   "Dijes",
+//   "Pulseras"
+// ];
+
+// Categories: Sophiluim - Iluminación Creativa
 export const CATEGORIES = [
-  "Aros",
-  "Cadenas",
-  "Conjuntos",
-  "Dijes",
-  "Pulseras"
+  "Controladores de Luz",
+  "Cables y Conectores",
+  "Focos Exteriores",
+  "Fuentes de Alimentación",
+  "Iluminación de Riel",
+  "Iluminación LED",
+  "Lámparas de Techo",
+  "Lámparas de Pared",
+  "Lámparas de Mesa",
+  "Lámparas de Pie",
+  "Lámparas Decorativas",
+  "Iluminación de Jardín",
+  "Iluminación para Piscinas",
+  "Iluminación de Oficina",
+  "Iluminación de Tiendas",
+  "Iluminación Industrial",
+  "Iluminación Solar"
 ];
